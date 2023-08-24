fe11d3abe75279c258507073e2e64eeefd7d3c06
kloeffler95 <kevin.loeffler@colorado.edu>
2023-08-23 20:14:59 -0600

    Fixing serviceworker.

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,6 +1,6 @@
 // This is the "Offline copy of pages" service worker
 
-const CACHE = "pwabuilder-offline";
+const CACHE = "quip-offline";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
@@ -17,21 +17,36 @@ workbox.routing.registerRoute(
   })
 );
 
-const status = await navigator.permissions.query({
-  // @ts-ignore
-  name: 'periodic-background-sync',
+// Listen the periodic background sync events to update the cached resources.
+self.addEventListener('periodicsync', event => {
+  if (event.tag === 'update-cached-content') {
+      event.waitUntil(updateCachedContent());
+  }
 });
 
-if (status.state === 'granted') {
-  navigator.serviceWorker.ready.then(async (sw: any) => {
-    await sw.periodicSync.register('periodicsync', {
-      minInterval: 24 * 60 * 60 * 1000,
-    });
-  })
-    .catch(error => {
-      console.error('[BackgroundSync] Error: ' + JSON.stringify(error, null, 2));
-    });
+async function updateCachedContent() {
+  const requests = await findCacheEntriesToBeRefreshed();
+  const cache = await caches.open(CACHE);
+
+  for (const request of requests) {
+      try {
+          // Fetch the new version.
+          const fetchResponse = await fetch(request);
+          // Refresh the cache.
+          await cache.put(request, fetchResponse.clone());
+      } catch (e) {
+          // Fail silently, we'll just keep whatever we already had in the cache.
+      }
+  }
 }
-else {
-  console.error('[BackgroundSync] Does not have permission');
+
+// Find the entries that are already cached and that we do want to update. This way we only
+// update these ones and let the user visit new pages when they are online to populate more things
+// in the cache.
+async function findCacheEntriesToBeRefreshed() {
+  const cache = await caches.open(CACHE);
+  const requests = await cache.keys();
+  return requests.filter(request => {
+      return !DONT_UPDATE_RESOURCES.some(pattern => request.url.includes(pattern));
+  });
 }
